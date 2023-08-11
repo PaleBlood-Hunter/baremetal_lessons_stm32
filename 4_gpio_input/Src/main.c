a3c2edf74be3895fa69e48ae7625764904369e10
Vinícius Jean Ferreira <109921345+PaleBlood-Hunter@users.noreply.github.com>
2023-08-11 16:07:53 -0300

    Add files via upload

---
+++ 4_gpio_input/Src/main.c
@@ -0,0 +1,25 @@
+#include "stm32f4xx.h"
+
+#define GPIOAEN			(1U<<0)
+#define PIN5			(1U<<5)
+#define	LED_PIN			PIN5
+
+
+int main(void)
+{
+	RCC->AHB1ENR |=GPIOAEN; //ENABLE THE CLOCK FOR PERIPHERAL A
+
+	GPIOA->MODER |=(1U<<10); //SET PIN A5 AS AN OUTPUT
+	GPIOA->MODER &=~(1U<<11);// THE SAME
+
+	while(1)
+	{
+		GPIOA->BSRR = LED_PIN;		//TOGGLE PIN 5
+		for(int i=0;i<100000;i++);	//DELAY
+
+		GPIOA->BSRR = 1U<<21;
+		for(int i=0;i<100000;i++);	//DELAY
+	}
+}
+
+
